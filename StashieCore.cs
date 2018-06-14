3a94685dcfe830a162647b71ad2c51582c195cc9
dxbydt <dxbydt@gmail.com>
2018-06-14 09:52:27 -0700

    Fixing nuget package references and a bug introduced in PoE 3.3

--- StashieCore.cs
+++ StashieCore.cs
@@ -251,6 +251,7 @@ namespace Stashie
                 if ( invItem.Item == null )
                     continue;
 
+                if (invItem.InventPosX > 11 || invItem.InventPosY > 4 || !invItem.Item.IsValid) continue;
                 if ( string.IsNullOrEmpty( invItem.Item.Path ) )
                 {
                     LogMessage( $"Bugged item detected on X:{invItem.InventPosX}, Y:{invItem.InventPosY}, skipping.. Change location to fix this or restart the game (exit to character selection).", 5 );
